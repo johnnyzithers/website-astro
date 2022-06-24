f6b5eb085e76979767471fdaa9a683bf52af3bf3
johnnyzithers <johnnyzithers@gmail.com>
2022-06-24 14:02:33 -0400

    netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import preact from '@astrojs/preact';
 
+// https://astro.build/config
 export default defineConfig({
-
-	adapter: netlify(),
-	site:'https://johnnyzithers.github.io'
+	integrations: [preact()],
 });
