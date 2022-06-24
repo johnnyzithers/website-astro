6aad638335ea9853dd73fa2469b3fcaa9279a2a3
johnnyzithers <johnnyzithers@gmail.com>
2022-06-24 12:15:14 -0400

    netlify adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
-// https://astro.build/config
 export default defineConfig({
+
+	adapter: netlify(),
 	site:'https://johnnyzithers.github.io'
 });
